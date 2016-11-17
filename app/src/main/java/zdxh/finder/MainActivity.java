e1bce43e4e97ca86515da257048ad48c07b3aca8
kind m <kind m>
2016-11-17 23:40:45 +0800

    First commit

---
+++ app/src/main/java/zdxh/finder/MainActivity.java
@@ -0,0 +1,52 @@
+package zdxh.finder;
+
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import zdxh.finder.fragment.CardFragment;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("手机用户", CardFragment.class)
+                .add("卡用户", CardFragment.class)
+                .add("卡用户", CardFragment.class)
+                .add("卡用户", CardFragment.class)
+                .add("卡用户", CardFragment.class)
+                .create());
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+
+
+
+    }
+}
